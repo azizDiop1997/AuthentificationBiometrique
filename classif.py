84494273ac90e25b148caa6766efa4a71ad3d383
Thomas <lescotetth@cy-tech.fr>
2022-06-13 16:06:12 +0200

    NN model complete

--- classif.py
+++ classif.py
@@ -16,10 +16,15 @@ from tensorflow.keras.models import Sequential
 
 import pathlib
 
+
+img_height = 320
+img_width = 120
+
 def setup():
 
 	dataset_url="http://0.0.0.0:8000/BDD_FingerVeins.tar.gz"
-	data_dir = tf.keras.utils.get_file(fname='BDD_FingerVeins', origin=dataset_url, untar=True)
+	# fname='BDD_FingerVeins'
+	data_dir = tf.keras.utils.get_file(fname='DB_clean', origin=dataset_url, untar=True)
 	print("data_dir: ",data_dir)
 	data_dir = pathlib.Path(data_dir)
 	#image_count = len(list(data_dir.glob('001left*/*.bmp')))
@@ -27,6 +32,7 @@ def setup():
 	#print("image_count: ",image_count)	
 	
 	imgs = list(data_dir.glob('*/*.bmp')) # trier toutes les images dans des sous dossiers
+	# bmp
 	x=2
 	print(PIL.Image.open(str(imgs[x])))
 	print(str(imgs[x]))
@@ -40,8 +46,8 @@ def setup():
 			image = cv2.imread(str(imgs[i]))
 			height, width = image.shape[:2]
 			#print(height, width,str(imgs[i]))
-			if((height, width)!=(240,320)):
-				print("error",str(imgs[i]),height, width)
+			if((img_height, img_width)!=(320,120)):
+				print("error",str(imgs[i]),img_height, img_width)
 				#exit(-1)
 		#else:
 			#print(i," rep ",str(imgs[i]))
@@ -49,8 +55,6 @@ def setup():
 	print("end")
 
 	batch_size = 32
-	img_height = 320
-	img_width = 240
 
 
 	train_ds = tf.keras.utils.image_dataset_from_directory(
@@ -75,19 +79,74 @@ def setup():
 	class_names = train_ds.class_names
 	print(class_names)
 
-	plt.figure(figsize=(10, 10))
-	for images, labels in train_ds.take(1):
-		for i in range(9):
-			ax = plt.subplot(3, 3, i + 1)
-			plt.imshow(images[i].numpy().astype("uint8"))
-			plt.title(class_names[labels[i]])
-			plt.axis("off")
-			print('ok')
+	print(train_ds)
+#	plt.figure(figsize=(10, 10))
+#	for images, labels in train_ds.take(1):
+#		for i in range(9):
+#			ax = plt.subplot(3, 3, i + 1)
+#			plt.imshow(images[i].numpy().astype("uint8"))
+#			plt.title(class_names[labels[i]])
+#			plt.axis("off")
+#			print('ok')
 
 #	for image_batch, labels_batch in train_ds:
 #		print(image_batch.shape)
 #		print(labels_batch.shape)
 #		break
+
+	
+# https://www.tensorflow.org/tutorials/load_data/images#standardize_the_data
+
+	
+	# normalization
+#	normalization_layer = tf.keras.layers.Rescaling(1./255)
+	
+#	normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+	
+#	print(normalized_ds)
+#	print("------")	
+#
+#	image_batch, labels_batch = next(iter(normalized_ds))
+#	print(image_batch, labels_batch )
+#	first_image = image_batch[0]
+	# Notice the pixel values are now in `[0,1]`.
+#	print(np.min(first_image), np.max(first_image))
+
+
+# 0.0 0.96902645
+
+
+	model = tf.keras.Sequential([
+  tf.keras.layers.Rescaling(1./255),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(126)
+])
+
+
+	model.compile(
+  optimizer='adam',
+  loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
+  metrics=['accuracy'])
+# ML : https://www.tensorflow.org/tutorials/images/classification
+
+
+	model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=3
+)
+
+
+
+
+
 
 setup()
 
--- tri.py
+++ tri.py
@@ -1,20 +1,24 @@
 import glob
 import os
 
-images = glob.glob('BDD_FingerVeins/*.bmp')
+images = glob.glob('DB_clean/*.bmp')
 print(images)
 # python3 tri.py | grep bmp | wc
+
 directories=[]
+x=9 # longueur chemin avant nom image
+# or use split
 for img in images: 
 	print(img)
-	short=img[16:-6]
-	if img[16:-6] not in directories:
-		os.system("mkdir {}".format(img[16:-6]))
-	os.system("mv {} {}".format(img,img[16:-6]))
+	short=img[x:-6]
+	print(short)
+	if img[x:-6] not in directories:
+		os.system("mkdir {}".format(img[x:-6]))
+	os.system("mv {} {}".format(img,img[x:-6]))
 
 
 
-os.system('mv 0*/ BDD_FingerVeins/')
+os.system('mv 0*/ DB_clean')
 
 # to adapt: / or use keras untar
 #os.system('cp -r BDD_FingerVeins /home/kali/.keras/datasets')
