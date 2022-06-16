10e0e752095df4f9813e334020dafcb98d989bc5
azizDiop1997 <95911654+azizDiop1997@users.noreply.github.com>
2022-06-16 13:40:21 -0400

    Update augmentation

--- pycode_augmentation_database/file.py
+++ pycode_augmentation_database/file.py
@@ -18,12 +18,12 @@ import Augmentor
 def augment() :
 	p = Augmentor.Pipeline(source_directory="../BDD_FingerVeins/", output_directory="../database/")
 	
-	p.random_distortion(probability=0.2,grid_width=4, grid_height=4, magnitude=8)
-	p.zoom(probability=0.2, min_factor=1.1, max_factor=1.6)
-	p.shear(probability=0.2, max_shear_left= 25, max_shear_right=1)
-	p.crop_random(probability=0.2, percentage_area=0.8)
-	p.flip_random(probability=0.2)
-	p.resize(probability=1, width=120, height=320)
+	p.random_distortion(probability=0.4,grid_width=6, grid_height=6, magnitude=8)
+	#p.zoom(probability=0.2, min_factor=1.1, max_factor=1.6)
+	#p.shear(probability=0.2, max_shear_left= 25, max_shear_right=1)
+	p.crop_random(probability=0.4, percentage_area=0.8)
+	p.flip_random(probability=0.4)
+	#p.resize(probability=1, width=120, height=320)
 	p.sample(10000)
 	
 augment()
